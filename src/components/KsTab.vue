6f612cd9935ce62db1cad1b7b800565def577f0d
Giovanni DeCristofaro <giovanni.decristofaro@crompco.com>
2017-03-14 14:00:31 -0400

    adding the ks tabs compoenent updates

--- src/components/KsTab.vue
+++ src/components/KsTab.vue
@@ -1,7 +1,7 @@
 <style></style>
 
 <template>
-	<div v-show="active">
+	<div v-show="active" :class="{'active-tab': active}">
 		<slot></slot>
 	</div>
 </template>
@@ -25,13 +25,28 @@
 
 		mounted() {
 			this.$nextTick(() => {
-				this.$parent.registerTab(this);
+
+				let parent = this.getTabsParent(this);
+
+				parent.registerTab(this);
+				parent.$on('tab-changed', (title) => {
+					if ( title != this.title ) {
+						this.active = false;
+					}
+				})
 			});
 		},
 
 		methods: {
 			setActive(active) {
 				this.active = active;
+			},
+			getTabsParent(vm) {
+				if ( vm.$parent.$options._componentTag === 'ks-tabs' ) {
+					return this.$parent;
+				}
+
+				return this.getTabsParent(this.$parent);
 			}
 		},
 
