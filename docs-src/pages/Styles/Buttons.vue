97aab2f7cd2611f4002d61e66482313802d3e5a2
tomwerkheiser <tawerkheiser@gmail.com>
2017-03-15 10:16:32 -0400

    change button styles

--- docs-src/pages/Styles/Buttons.vue
+++ docs-src/pages/Styles/Buttons.vue
@@ -2,12 +2,31 @@
 	<div class="panel">
 		<div class="panel-header">Buttons</div>
 		<div class="panel-body">
-			<button class="button">
-				I'm a button
-			</button>
-			<a href="#" class="button">
-				I'm a button
-			</a>
+			<div class="row">
+				<div>Default</div>
+				<hr>
+				<div class="sm-2">
+					<button class="button">
+						I'm a button
+					</button>
+					<a href="#" class="button">
+						I'm a button
+					</a>
+				</div>
+			</div>
+			<!--<br>-->
+			<!--<div class="row">-->
+				<!--<div>Outline</div>-->
+				<!--<hr>-->
+				<!--<div class="sm-2">-->
+					<!--<button class="button outline">-->
+						<!--I'm a button-->
+					<!--</button>-->
+					<!--<a href="#" class="button outline">-->
+						<!--I'm a button-->
+					<!--</a>-->
+				<!--</div>-->
+			<!--</div>-->
 		</div>
 	</div>
 </template>
