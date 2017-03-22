93935f3c025b201722926d906662b8b8bb5316f2
Giovanni DeCristofaro <gdecris@gmail.com>
2017-03-22 14:18:41 -0400

    adding skeleton calendar component

---
+++ src/components/KsCalendar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="ks-calendar">
+        <div class="ks-calendar-title">
+
+        </div>
+
+        <div class="ks-calendar-view">
+            <div class="ks-calendar-controls"></div>
+            
+            <div class="ks-calendar-month">
+
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'KsCalendar',
+
+        props: {
+
+        },
+
+        data() {
+            return {
+
+            };
+        }
+    }
+</script>
