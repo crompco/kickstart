82b6e4e4f1589c91ce96d4a5d59e37f7d4e7942e
tomwerkheiser <tom.werkheiser@crompco.com>
2017-03-24 16:45:08 -0400

    start of responsive navigation

--- docs-src/pages/Styles/TopBar.vue
+++ docs-src/pages/Styles/TopBar.vue
@@ -1,14 +1,25 @@
 <template>
 	<div class="top-bar">
-		<ul class="dropdown right">
-			<li>
-				<a href="#">tom.werkheiser@crompco.com</a>
-				<ul class="sub-dropdown">
-					<li><a href="#">Profile</a></li>
-					<li><a href="#">Sign Out</a></li>
-				</ul>
-			</li>
-		</ul>
+		<div class="left">
+			<ul class="menu">
+				<li><a href="#">One</a></li>
+				<li><a href="#">Two</a></li>
+				<li><a href="#">Three</a></li>
+				<li><a href="#">Four</a></li>
+				<li><a href="#">Five</a></li>
+			</ul>
+		</div>
+		<div class="right">
+			<ul class="dropdown">
+				<li>
+					<a href="#">tom.werkheiser@crompco.com</a>
+					<ul class="sub-dropdown">
+						<li><a href="#">Profile</a></li>
+						<li><a href="#">Sign Out</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
