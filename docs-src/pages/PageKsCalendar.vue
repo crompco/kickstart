fc7c8fc41fd5a719b58b98ecbb8c3749bad323ff
Giovanni <gdecris@gmail.com>
2017-03-27 15:12:18 -0400

    Calendar (#5)
    * adding skeleton calendar component
    
    * basic calendar functionality added
    
    * refactored a bit and added scss component file
    
    * minor tweaks to the format
    
    * converted table to flexbox instead
    
    * added more enhancements to the calendar and started the datepicker
    component
    
    * updating the calendar and datepicker components. still needs work but
    functional
    
    * removing unnecessary event propagation check

---
+++ docs-src/pages/PageKsCalendar.vue
@@ -0,0 +1,59 @@
+<template>
+	<div>
+		<div class="basic-block">
+			<h3>Datepicker</h3>
+			<ks-datepicker v-model="date1"></ks-datepicker>
+		</div>
+		<div class="basic-block">
+			<h3>Plain Calendar with Event Slot</h3>
+			<ks-calendar>
+				<div :slot="'2017-03-22'">
+					<strong>Patty's Birthday</strong>
+				</div>
+			</ks-calendar>
+		</div>
+		<div class="basic-block">
+			<h3>Custom Week Start</h3>
+			<ks-calendar
+				week-start="1"
+			    week-height="80px"
+			></ks-calendar>
+		</div>
+	</div>
+</template>
+
+
+<script>
+	import KsCalendar from '../../src/components/KsCalendar';
+	import KsDatepicker from '../../src/components/KsDatepicker';
+
+	export default {
+		name: 'PageKsCalendar',
+
+		props: {},
+
+		data() {
+			return {
+				date1: '2017-01-01',
+				bday: "2017-03-22"
+			};
+		},
+
+		computed: {},
+
+		mounted() {
+
+		},
+
+		methods: {},
+
+		watch: {},
+
+		components: {
+			KsCalendar,
+			KsDatepicker,
+		}
+	}
+</script>
+
+<style></style>
---
+++ src/helpers/dates.js
@@ -0,0 +1,170 @@
+import {pad_left} from './strings';
+
+export const defaultLocale = {
+	months: {
+		names: [
+			"January", "February", "March", "April", "May", "June",
+			"July", "August", "September", "October", "November", "December"
+		],
+		abbreviations: [
+			'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+			'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+		]
+	},
+	days: {
+		names: [
+			"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+		],
+		abbreviations: [
+			"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
+		]
+	}
+};
+
+export function getMonthRange(date) {
+	let year = date.getFulllYear();
+	let month = date.getMonth();
+	return {
+		first: new Date(y, m, 1),
+		last: new Date(y, m + 1, 0)
+	}
+}
+
+/**
+ * @param {int} The month number, 0 based
+ * @param {int} The year, not zero based, required to account for leap years
+ * @return {Date[]} List with date objects for each day of the month
+ */
+export function getDaysInMonth(month, year) {
+	var date = new Date(year, month, 1);
+	var days = [];
+	while (date.getMonth() === month) {
+		days.push(new Date(date));
+		date.setDate(date.getDate() + 1);
+	}
+	return days;
+}
+
+/**
+ *
+ * @param date
+ * @return {Date}
+ */
+export function cloneDate(date) {
+	return new Date(date.getTime())
+}
+
+/**
+ *
+ * @param date
+ * @param days
+ */
+export function subDays(date, days = 1) {
+	let d = cloneDate(date);
+	d.setDate(date.getDate() - days);
+	if ( d.getDate() > date.getDate() ) {
+		d.setMonth(date.getMonth() - 1);
+		if ( d.getMonth() > date.getMonth() ) {
+			d.setFullYear(date.getFullYear() - 1);
+		}
+	}
+	return d;
+}
+
+/**
+*
+* @param date
+* @param months
+*/
+export function subMonths(date, months = 1) {
+	let d = cloneDate(date);
+	d.setMonth(date.getMonth() - months);
+	if ( d.getMonth() > date.getMonth() ) {
+		d.setFullYear(date.getFullYear() - 1);
+	}
+	return d;
+}
+
+/**
+ *
+ * @param date
+ * @param years
+ */
+export function subYears(date, years = 1) {
+	let d = cloneDate(date);
+	d.setFullYear(date.getFullYear() - years);
+	return d;
+}
+
+/**
+ *
+ * @param date
+ * @param days
+ */
+export function addDays(date, days = 1) {
+	let d = cloneDate(date);
+	d.setDate(date.getDate() + days);
+	if ( d.getDate() < date.getDate() ) {
+		d.setMonth(date.getMonth() + 1);
+		if ( d.getMonth() < date.getMonth() ) {
+			d.setFullYear(date.getFullYear() + 1);
+		}
+	}
+	return d;
+}
+
+/**
+ *
+ * @param date
+ * @param months
+ */
+export function addMonths(date, months = 1) {
+	let d = cloneDate(date);
+	d.setMonth(date.getMonth() + months);
+	if ( d.getMonth() < date.getMonth() ) {
+		d.setFullYear(date.getFullYear() + 1);
+	}
+	return d;
+}
+
+/**
+ * Formats date
+ * replace Y, m, and d chars
+ *
+ * @param date
+ * @return {string}
+ */
+export function formatDate(date, format = 'Y-m-d') {
+	if ( !(date instanceof Date) ) {
+		date = parseDate(date, format);
+	}
+	let year = date.getFullYear();
+	let month = date.getMonth() + 1;
+	let day = date.getUTCDate();
+
+	month = pad_left(month, '0', 2);
+	day = pad_left(day, '0', 2);
+
+	return format.toLowerCase().replace('y', year).replace('m', month).replace('d', day);
+}
+
+/**
+ *
+ * @param date
+ * @param format
+ * @return {Date|*}
+ */
+export function parseDate(date, format = 'Y-m-d') {
+	// TODO handle different formats
+	let d = date.split('-');
+	date = (new Date());
+	date.setFullYear(d[0]);
+	date.setMonth(d[1]-1);
+	date.setDate(d[2]);
+
+	return date;
+}
+
+export default {
+	defaultLocale
+}
