908b9405c298455129b6475e627dd8485ddbd6e2
tomwerkheiser <tom.werkheiser@crompco.com>
2017-04-21 10:20:43 -0400

    add build script

--- build/release.sh
+++ build/release.sh
@@ -14,8 +14,22 @@ echo
 if [[ $REPLY =~ [Yy]$ ]]; then
     echo "Releasing $VERSION ..."
 
+    # Build
     npm run build:all
 
+    # Set new version to package.json
+    npm --no-git-tag-version version $VERSION
+
+    # Commit the build and new version
+    git add -A
+    git commit -m "[release] $VERSION"
+
+    # Tag release
+    git tag -a "v$VERSION" -m "$VERSION"
+
+    # Push to remote
+    git push origin refs/tags/v$VERSION
+    git push
 else
     echo "Version $VERSION was not released."
 fi
