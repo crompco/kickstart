f4a86ab26021540220d213a44ba593a235013e4c
tomwerkheiser <tom.werkheiser@crompco.com>
2017-06-08 11:51:52 -0400

    add docs to grid page

--- docs-src/pages/PageKsGrid.vue
+++ docs-src/pages/PageKsGrid.vue
@@ -37,13 +37,63 @@
             </table>
         </div>
 
-        <div>
+        <h3>No Row Collapse</h3>
+        <div class="row col">
             <div class="col tab-6" style="background:#fff;padding:1rem;">
                 Here is col 1 of tab grid
             </div>
             <div class="col tab-6" style="background:#fff;padding:1rem;">
                 Here is col 2 of tab grid
             </div>
+        </div>
+
+        <h3>Row Collapse</h3>
+        <div class="row col row-collapse">
+            <div class="col tab-6" style="background:#fff;padding:1rem;">
+                Here is col 1 of tab grid
+            </div>
+            <div class="col tab-6" style="background:#fff;padding:1rem;">
+                Here is col 2 of tab grid
+            </div>
+        </div>
+
+        <div class="row">
+            <ks-tabs>
+                <ks-tab title="Helpers">
+                    <table class="table striped">
+                        <thead>
+                            <tr>
+                                <th>Class</th>
+                                <th>Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>.row</td>
+                                <td>This will just add a row around the columns. This adds a clearfix.</td>
+                            </tr>
+                            <tr>
+                                <td>.{col-prefix}-half</td>
+                                <td>This is an alias for .{col-prefix}-6</td>
+                            </tr>
+                            <tr>
+                                <td>.{col-prefix}-quarter</td>
+                                <td>This is an alias for .{col-prefix}-3</td>
+                            </tr>
+                            <tr>
+                                <td>.{col-prefix}-full</td>
+                                <td>This is an alias for .{col-prefix}-12</td>
+                            </tr>
+                            <tr>
+                                <td>.row-collapse</td>
+                                <td>
+                                    This is a helper to collapse the row. If the margin on each side is too much or is off, collapsing the row will remove the margin on both sides. This is mainly used when nesting columns.
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </ks-tab>
+            </ks-tabs>
         </div>
     </div>
 </template>
