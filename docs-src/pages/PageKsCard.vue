c57e5f853d4693d11b8415edb10a22b3b495b869
tomwerkheiser <tom.werkheiser@crompco.com>
2017-06-08 12:11:10 -0400

    docs for ks card

--- docs-src/pages/PageKsCard.vue
+++ docs-src/pages/PageKsCard.vue
@@ -1,20 +1,46 @@
 <template>
     <div>
         <h2>Card</h2>
-        <div class="sm-4">
-            <div class="ks-card">
-                <div class="card-header">
-                    Foo
-                </div>
-                <div class="card-body">
-                    Here is the body of the card. <br>
-                    Let's break it down!
-                </div>
-                <div class="card-footer">
-                    <button>Edit</button>
-                    <button class="danger">Delete</button>
+        <div class="row col">
+            <div class="sm-4">
+                <div class="ks-card">
+                    <div class="card-header">
+                        Foo
+                    </div>
+                    <div class="card-body">
+                        Here is the body of the card. <br>
+                        Let's break it down!
+                    </div>
+                    <div class="card-footer">
+                        <button>Edit</button>
+                        <button class="danger">Delete</button>
+                    </div>
                 </div>
             </div>
+        </div>
+
+        <div class="alert info">
+            This is not a component. Just uses css classes to style
+        </div>
+
+        <div class="row">
+            <code-block>
+                <code class="language-html" v-pre>
+                    &lt;div class="ks-card">
+                        &lt;div class="card-header">
+                            Foo
+                        &lt;/div>
+                        &lt;div class="card-body">
+                            Here is the body of the card. &lt;br>
+                            Let's break it down!
+                        &lt;/div>
+                        &lt;div class="card-footer">
+                            &lt;button>Edit&lt;/button>
+                            &lt;button class="danger">Delete&lt;/button>
+                        &lt;/div>
+                    &lt;/div>
+                </code>
+            </code-block>
         </div>
     </div>
 </template>
