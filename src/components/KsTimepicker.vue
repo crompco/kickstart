09b2d181bd759d7f8ccfa033e64b409baa9d35e9
Giovanni DeCristofaro <=>
2018-01-03 20:31:37 -0500

    adding some small updates that will likely need to be refactored

--- src/components/KsTimepicker.vue
+++ src/components/KsTimepicker.vue
@@ -1,14 +1,19 @@
 <template>
     <div class="ks-timepicker">
-        <input type="text" v-model="display_value">
+        <input type="text" v-model="display_value" @input="">
         <input type="hidden" :name="name">
-        <ks-tooltip>
-
-        </ks-tooltip>
+        <!--<ks-tooltip trigger="click">-->
+            <!--<ul>-->
+                <!--<li>09:00</li>-->
+                <!--<li>10:00</li>-->
+            <!--</ul>-->
+        <!--</ks-tooltip>-->
     </div>
 </template>
 
 <script>
+
+    import KsTooltip from './KsTooltip';
 
     export default {
         name: 'KsTimepicker',
@@ -33,7 +38,7 @@
         computed: {
             timeOptions() {
 
-            }
+            },
         },
 
         data() {
@@ -43,22 +48,51 @@
         },
 
         methods: {
+
             formatTimeValue(time, format) {
                 let time_info = this.parseTime(time);
-                format.replace('H', )
+                return format.replace('H', time_info.full_hour)
+                    .replace('h', time_info.hour)
+                    .replace('i', time_info.minute)
+                    .replace('a', time_info.meridiem.toLowerCase())
+                    .replace('A', time_info.meridiem.toUpperCase());
             },
+
             parseTime(time) {
                 let time_parts = time.match(/([0-9]{1,2})\:([0-9]{1,2})\s*(am|pm)/i);
+
+                // Setup the time parts
                 let hour = time_parts[1];
                 let minute = time_parts[2] || '00';
                 let meridiem = time_parts[3] || '';
                 let military = meridiem ? false : true;
-                return {
-                    hour, minute, meridiem, military
+                let full_hour;
+
+                if ( military ) {
+                    full_hour = meridiem.toLowerCase() == 'pm' ? hour + 12 : hour;
+                } else {
+                    full_hour = hour;
                 }
+                return {
+                    hour, minute, meridiem, military, full_hour
+                };
             }
         },
 
-        components: {}
+        components: {
+            KsTooltip
+        },
+
+        watch: {
+            displayFormat() {
+                this.display_value = this.formatTimeValue(this.value, this.displayFormat);
+            },
+            timeFormat() {
+                this.$emit('input', this.formatTimeValue(this.value, this.timeFormat))
+            },
+            value() {
+
+            }
+        }
     }
 </script>
