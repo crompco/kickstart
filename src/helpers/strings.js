59ae6924455762f0240f9623a6bb90d8ce1aaeb8
Tom Werkheiser <tom.werkheiser@crompco.com>
2021-01-14 16:42:31 -0500

    fix currency filter with commas

--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -44,7 +44,9 @@ export function posesses(str) {
  * @returns {string}
  */
 export function currency(str, symbol = '$') {
-	return symbol + parseFloat(str).toFixed(2).replace(/./g, function(c, i, a) {
+	return symbol + parseFloat(str.replace(/,/g, ''))
+		.toFixed(2)
+		.replace(/./g, function(c, i, a) {
 			return i && c !== "." && ((a.length - i) % 3 === 0) ? ',' + c : c;
 		});
 }
