2d5f8015eb0782a3c34bbccb9e2168e92ab07fe8
Giovanni DeCristofaro <gdecris@gmail.com>
2021-01-21 10:57:24 -0500

    adding muted badge

--- docs-src/pages/Styles/Badge.vue
+++ docs-src/pages/Styles/Badge.vue
@@ -8,6 +8,7 @@
             <div class="ks-badge info">10</div>
             <div class="ks-badge warning">10</div>
             <div class="ks-badge error">10</div>
+            <div class="ks-badge muted">10</div>
             <br>
             <br>
             <div class="ks-badge">Tag</div>
@@ -15,6 +16,7 @@
             <div class="ks-badge info">Tag</div>
             <div class="ks-badge warning">Tag</div>
             <div class="ks-badge error">Tag</div>
+            <div class="ks-badge muted">Tag</div>
 
             <code-block style="margin-top:1rem;">
                 <template v-pre>
@@ -23,12 +25,14 @@
                     &lt;div class="ks-badge info">10&lt;/div>
                     &lt;div class="ks-badge warning">10&lt;/div>
                     &lt;div class="ks-badge error">10&lt;/div>
+                    &lt;div class="ks-badge muted">10&lt;/div>
                     <br>
                     &lt;div class="ks-badge">Tag&lt;/div>
                     &lt;div class="ks-badge success">Tag&lt;/div>
                     &lt;div class="ks-badge info">Tag&lt;/div>
                     &lt;div class="ks-badge warning">Tag&lt;/div>
                     &lt;div class="ks-badge error">Tag&lt;/div>
+                    &lt;div class="ks-badge muted">Tag&lt;/div>
                 </template>
             </code-block>
         </div>
