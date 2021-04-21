4b918c35da4e84c88e550b2a1c1eb041f865a9fa
Tom Werkheiser <tom.werkheiser@crompco.com>
2021-04-21 16:26:51 -0400

    remove unneeded code

--- docs/functions/countries.js
+++ docs/functions/countries.js
@@ -20,22 +20,3 @@ exports.handler = async (event, context) => {
         })
     }
 }
-
-// export default function (req, res) {
-//     const protocol = req.connection?.encrypted ? 'https' : 'http';
-//     const baseUrl = `${protocol}://${req.headers.host}/`;
-//     const search = new URL(req.url, baseUrl);
-//     const params = search.searchParams;
-//
-//     const name_regex = new RegExp('^.*' + escapeRegExp(params.get('q')) + '.*', 'i');
-//     const limit = 10;
-//     const page = parseInt(typeof params.get('page') != 'undefined' ? params.get('page') : 1);
-//     const slice_start = (page - 1) * limit;
-//
-//     const json = JSON.stringify({
-//         data: countries.filter(o => !!o.name.match(name_regex))
-//             .slice(slice_start, slice_start + limit)
-//     });
-//
-//     res.end(json)
-// }
