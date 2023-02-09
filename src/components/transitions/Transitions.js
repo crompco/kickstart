85bfeb0f045d2c41504400abcadb784f7aebdd82
Tom Werkheiser <tom.werkheiser@crompco.com>
2023-02-09 11:42:31 -0500

    change slide transition to component and move to methods

--- src/components/transitions/Transitions.js
+++ src/components/transitions/Transitions.js
@@ -4,9 +4,43 @@ import Velocity from 'velocity-animate';
 // Internal Dependencies
 
 const SlideTransition = {
-    functional: true,
+    methods: {
+        beforeEnter(el) {
+            el.style.display = '';
+            el.style.maxHeight = 'none';
+            el.style.overflow = 'hidden';
+            el.style.opacity = '0';
+        },
 
-    render(createElement, context) {
+        enter(el, done) {
+            const height = el.offsetHeight;
+
+            Velocity(el, {opacity: 1, maxHeight: `${height}px`}, {duration: 400, complete: done})
+        },
+
+        afterEnter(el) {
+            el.style.removeProperty('overflow');
+            el.style.removeProperty('max-height');
+        },
+
+        beforeLeave(el) {
+            el.style.overflow = 'hidden';
+            el.style.maxHeight = `${el.offsetHeight}px`;
+        },
+
+        leave(el, done) {
+            Velocity(el, {opacity: 0, maxHeight: 0, paddingTop: 0, paddingBottom: 0}, {duration: 400, complete: done})
+        },
+
+        afterLeave(el) {
+            el.style.removeProperty('padding-top');
+            el.style.removeProperty('padding-bottom');
+            el.style.removeProperty('overflow');
+            el.style.removeProperty('max-height');
+        },
+    },
+
+    render(createElement) {
         return createElement(
             'transition',
             {
@@ -15,44 +49,16 @@ const SlideTransition = {
                     mode: 'in-out',
                     css: false,
                 },
-
                 on: {
-                    beforeEnter(el) {
-                        el.style.display = '';
-                        el.style.maxHeight = 'none';
-                        el.style.overflow = 'hidden';
-                        el.style.opacity = '0';
-                    },
-
-                    enter(el, done) {
-                        const height = el.offsetHeight;
-
-                        Velocity(el, {opacity: 1, maxHeight: `${height}px`}, {duration: 400, complete: done});
-                    },
-
-                    afterEnter(el) {
-                        el.style.removeProperty('overflow');
-                        el.style.removeProperty('max-height');
-                    },
-
-                    beforeLeave(el) {
-                        el.style.overflow = 'hidden';
-                        el.style.maxHeight = `${el.offsetHeight}px`;
-                    },
-
-                    leave(el, done) {
-                        Velocity(el, {opacity: 0, maxHeight: 0, paddingTop: 0, paddingBottom: 0}, {duration: 400, complete: done});
-                    },
-
-                    afterLeave(el) {
-                        el.style.removeProperty('padding-top');
-                        el.style.removeProperty('padding-bottom');
-                        el.style.removeProperty('overflow');
-                        el.style.removeProperty('max-height');
-                    },
-                }
+                    beforeEnter: this.beforeEnter,
+                    enter: this.enter,
+                    afterEnter: this.afterEnter,
+                    beforeLeave: this.beforeLeave,
+                    leave: this.leave,
+                    afterLeave: this.afterLeave
+                },
             },
-            context.children
+            this.$slots.default
         );
     }
 };
