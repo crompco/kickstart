69c6637e7e5a3e45d9f87d72613a060873e27865
Mikhail <mikhail.bezruchko@crompco.com>
2024-01-16 13:48:29 -0500

    add 'end of day' option to datepicker

--- docs/components/ComponentExamples/PageKsTimepicker.vue
+++ docs/components/ComponentExamples/PageKsTimepicker.vue
@@ -8,12 +8,12 @@
                     :time-format="time_format"
                     display-format="h:i A"
                     time-step="30"
-                    v-model="start_time"
+                    v-model="start_time_1"
                 ></ks-timepicker>
             </div>
             <pre>display format: h:i A</pre>
             <pre>format: {{time_format}}</pre>
-            <pre>v-model: {{start_time}}</pre>
+            <pre>v-model: {{start_time_1}}</pre>
 
             <code-block>
                 <template v-pre>
@@ -37,12 +37,12 @@
                     min-time="00:00"
                     display-format="H:i"
                     time-step="30"
-                    v-model="start_time"
+                    v-model="start_time_2"
                 ></ks-timepicker>
             </div>
             <pre>display format: H:i</pre>
             <pre>format: {{time_format}}</pre>
-            <pre>v-model: {{start_time}}</pre>
+            <pre>v-model: {{start_time_2}}</pre>
 
             <code-block>
                 <template v-pre>
@@ -53,6 +53,36 @@
                         display-format="H:i"
                         time-step="30"
                         v-model="start_time"
+                    >&lt;/ks-timepicker>
+                </template>
+            </code-block>
+        </div>
+
+        <div class="basic-block">
+            <div class="form-group">
+                <div class="label">Timepicker</div>
+                <ks-timepicker
+                    name="start_time"
+                    :time-format="time_format"
+                    display-format="h:i A"
+                    time-step="30"
+                    v-model="start_time_3"
+                    :end-of-day="true"
+                ></ks-timepicker>
+            </div>
+            <pre>display format: h:i A</pre>
+            <pre>format: {{ time_format }}</pre>
+            <pre>v-model: {{ start_time_3 }}</pre>
+
+            <code-block>
+                <template v-pre>
+                    &lt;ks-timepicker
+                    name="start_time"
+                    :time-format="time_format"
+                    display-format="h:i A"
+                    time-step="30"
+                    v-model="start_time"
+                    :end-of-day="true"
                     >&lt;/ks-timepicker>
                 </template>
             </code-block>
@@ -71,7 +101,9 @@
         data() {
             return {
                 time_format: 'H:i',
-                start_time: ''
+                start_time_1: '',
+                start_time_2: '',
+                start_time_3: '',
             }
         },
 
