6f2d09fd5bc43ee6d6b668f80bdc75542d4aea56
Giovanni DeCristofaro <giovanni.decristofaro@owlservices.com>
2025-07-30 10:33:44 -0400

    Update `KsStyleButtons` component: enhance examples, add color table with hover states, refactor disabled button text.
    - Fixed Ks button style to properly support background and text color utilities.

--- docs-src/components/StyleExamples/KsStyleButtons.vue
+++ docs-src/components/StyleExamples/KsStyleButtons.vue
@@ -84,7 +84,7 @@
                 <hr>
                 <div class="sm-12">
                     <button class="ks-button none disabled">
-                        I'm a default button
+                        I'm a disabled button
                     </button>
                     <a href="#" class="ks-button none tiny">
                         I'm a tiny button
@@ -103,7 +103,7 @@
             <code-block class="mt-2">
                 <template v-pre>
                     &lt;button class="ks-button none disabled">
-                        I'm a default button
+                        I'm a disabled button
                     &lt;/button>
                     &lt;a href="#" class="ks-button none tiny">
                         I'm a tiny button
@@ -116,16 +116,73 @@
                     &lt;/a>
                 </template>
             </code-block>
+            <div>
+                <table class="ks-table striped shadow">
+                    <thead>
+                        <tr>
+                            <th>Class</th>
+                            <th>Hover Class</th>
+                            <th>Value</th>
+                            <th>Example</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <template v-for="color in colors">
+                            <template v-if="typeof color.colors === 'string'">
+                                <tr>
+                                    <td><code>.bg-{{color.name}} (.text-red-500)</code></td>
+                                    <td></td>
+                                    <td><code>{{color.colors}}</code></td>
+                                    <td>
+                                        <button :class="`ks-button bg-${color.name} text-red-500`">{{color.name}}</button>
+                                    </td>
+                                </tr>
+                            </template>
+                            <template v-else v-for="(hex, weight) in color.colors">
+                                <tr>
+                                    <td>
+                                        <code>.bg-{{color.name}}-{{weight}}</code>
+                                    </td>
+                                    <td>
+                                        <code>.hover:bg-{{color.name}}-{{getHoverWeight(weight)}}</code>
+                                    </td>
+                                    <td><code>{{hex}}</code></td>
+                                    <td>
+                                        <button :class="`ks-button bg-${color.name}-${weight} hover:bg-${color.name}-${getHoverWeight(weight)}`">
+                                            bg-{{color.name}}-{{weight}}
+                                        </button>
+                                    </td>
+                                </tr>
+                            </template>
+                        </template>
+                    </tbody>
+
+                </table>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-    // External Dependencies
-
-    // Internal Dependencies
+    import colors from '../../content/style-maps/colors.json';
 
     export default {
         name: 'Buttons',
+
+        data() {
+            return {
+                colors: colors.colors,
+            }
+        },
+
+        methods: {
+            getHoverWeight(weight) {
+                if ( weight === '900' ) {
+                    return parseInt(weight) - 100;
+                }
+
+                return parseInt(weight) + 100;
+            }
+        }
     }
 </script>
